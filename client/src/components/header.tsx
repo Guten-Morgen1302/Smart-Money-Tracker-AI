20bfdd0172c7a81384ac6447c528329648aa1075
callmeson <99388058+Guten-Morgen1302@users.noreply.github.com>
2025-03-15 13:08:27 +0530

    Update header.tsx

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from "@/components/ui/dialog"; 
 import { useToast } from "@/hooks/use-toast";
 
 type HeaderProps = {
