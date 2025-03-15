858be2ce828190be83c946109ec4445f814dc7f2
callmeson <99388058+Guten-Morgen1302@users.noreply.github.com>
2025-03-15 13:04:41 +0530

    Update login.tsx

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useLocation } from 'wouter';
+import { useLocation } from 'wouter'; 
 import { useToast } from '@/hooks/use-toast';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
